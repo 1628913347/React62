813ba63e9ae5de020e171709259f9bdc39e18de2
liguojin <1628913347@qq.com>
2019-03-09 20:31:01 +0800

    完成登录基本流程

--- src/login.js
+++ src/login.js
@@ -1,44 +1,80 @@
 // 登录页
 import React from 'react';
 import { Form, Icon, Button, Divider } from 'semantic-ui-react';
+import { withRouter } from "react-router-dom";
+import axios from 'axios';
 import './login.css';
 
 class Login extends React.Component {
-    render () {
-        return (
-            <div className="login-container">
-               <div className="login-logo">
-                    <Icon name='home' size='huge' />
-               </div>
-               <div className="login-form">
-                    <Form>
-                        <Form.Input 
-                            icon='user' 
-                            required 
-                            size='big' 
-                            iconPosition='left' 
-                            name='username'
-                            placeholder='请输入用户名...' 
-                            /> 
-                        <Form.Input 
-                            icon='user' 
-                            required 
-                            size='big' 
-                            iconPosition='left' 
-                            name='username'
-                            placeholder='请输入密码...' 
-                        /> 
-                        <Button fluid color="green">登录</Button>
-                        <Divider horizontal>---</Divider>
-                    </Form>
-                </div>
-                <div className="login-third">
-                <Icon name='qq' size='big' />
-                <Icon name='weixin' size='big' />
-                </div>
-            </div>
-           
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
     }
+  }
+  handleUsername = (event) => {
+    this.setState({
+      username: event.target.value
+    });
+  }
+  handlePassword = (event) => {
+    this.setState({
+      password: event.target.value
+    });
+  }
+  submit = async () => {
+    let url = 'http://47.96.21.88:8086/users/login';
+    let ret = await axios.post(url, {
+      uname: this.state.username,
+      pwd: this.state.password
+    });
+    // 存储token
+    sessionStorage.setItem('mytoken', ret.data.data.token);
+    // 获取history对象
+    let {history} = this.props;
+    // 跳转到主页
+    history.push('/home');
+  }
+  render() {
+    return (
+      <div className='login-container'>
+        <div className="login-logo">
+          <Icon name='home' size='huge' />
+        </div>
+        <div className="login-form">
+          <Form>
+            <Form.Input 
+              icon='user' 
+              required 
+              size='big' 
+              iconPosition='left' 
+              name='username'
+              value={this.state.username}
+              onChange={this.handleUsername}
+              placeholder='请输入用户名...' 
+            />
+            <Form.Input 
+              icon='user' 
+              required 
+              size='big' 
+              iconPosition='left' 
+              name='password'
+              value={this.state.password}
+              onChange={this.handlePassword}
+              placeholder='请输入密码...' 
+            />
+            <Button onClick={this.submit} fluid color='green'>登录</Button>
+            <Divider horizontal>---</Divider>
+          </Form>
+        </div>
+        <div className="login-third">
+          <Icon name='weixin' size='big' />
+          <Icon name='qq' size='big' />
+        </div>
+      </div>
+    );
+  }
 }
-export default Login;
+
+export default withRouter(Login);
