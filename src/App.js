aa6ff6883f9183b935f4a12f74dfd730ed4d1d73
liguojin <1628913347@qq.com>
2019-03-12 09:43:04 +0800

    完成字体图标

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+import './assets/style.css';
 
 import Login from './login';
 import Main from './module/main';
--- src/module/main.js
+++ src/module/main.js
@@ -1,5 +1,6 @@
 // 整体布局
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import './main.css';
 
 class Main extends React.Component {
@@ -10,7 +11,34 @@ class Main extends React.Component {
                 
                 </div>
                 <div className='home-menu'> 
-                
+                   <Grid columns={4} divided>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <div className="placeholder">
+                                    <i className='iconfont icon-happy2'></i>
+                                    <div>菜单</div>
+                                </div>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <div className="placeholder">
+                                    <i className='iconfont icon-wink2'></i>
+                                    <div>菜单</div>
+                                </div>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <div className="placeholder">
+                                    <i className='iconfont icon-grin'></i>
+                                    <div>菜单</div>
+                                </div>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <div className="placeholder">
+                                    <i className='iconfont icon-angry2'></i>
+                                    <div>菜单</div>
+                                </div>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </div>
             </div>
         );
