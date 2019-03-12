c3977c93e22efe9a654ec9c5aadb8498aa618784
liguojin <1628913347@qq.com>
2019-03-12 10:57:50 +0800

    完成菜单路由

--- src/module/main.js
+++ src/module/main.js
@@ -1,41 +1,56 @@
 // 整体布局
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import { Route, Link, Switch } from "react-router-dom";
 import './main.css';
+import { from } from 'rxjs';
 
 class Main extends React.Component {
     render () {
         return (
             <div className='home-container'> 
                 <div className='home-content'>
-                
+                    <Switch>
+                        <Route path='/home/main' render={()=><div>home</div>}/>
+                        <Route path='/home/info' render={()=><div>info</div>}/>
+                        <Route path='/home/chait' render={()=><div>chat</div>}/>
+                        <Route path='/home/my' render={()=><div>my</div>}/>
+                    </Switch>
                 </div>
                 <div className='home-menu'> 
                    <Grid columns={4} divided>
-                        <Grid.Row>
+                        <Grid.Row> 
                             <Grid.Column>
-                                <div className="placeholder">
-                                    <i className='iconfont icon-happy2'></i>
-                                    <div>菜单</div>
-                                </div>
+                                <Link to= '/home/main'>
+                                    <div className="placeholder">
+                                        <i className='iconfont icon-happy2'></i>
+                                        <div>菜单</div>
+                                    </div>
+                                </Link>
                             </Grid.Column>
                             <Grid.Column>
+                            <Link to= '/home/info'>
                                 <div className="placeholder">
                                     <i className='iconfont icon-wink2'></i>
                                     <div>菜单</div>
                                 </div>
+                            </Link>
                             </Grid.Column>
                             <Grid.Column>
+                            <Link to= '/home/chait'>
                                 <div className="placeholder">
                                     <i className='iconfont icon-grin'></i>
                                     <div>菜单</div>
                                 </div>
+                            </Link>
                             </Grid.Column>
                             <Grid.Column>
+                            <Link to= '/home/my'>
                                 <div className="placeholder">
                                     <i className='iconfont icon-angry2'></i>
                                     <div>菜单</div>
                                 </div>
+                            </Link>
                             </Grid.Column>
                         </Grid.Row>
                     </Grid>
