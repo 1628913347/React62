20cd454548e27895cce02ced8a167ce56ae73919
liguojin <1628913347@qq.com>
2019-03-12 20:12:10 +0800

    完成菜单链接样式动态控制

--- src/module/main.js
+++ src/module/main.js
@@ -3,17 +3,36 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import { Route, Link, Switch } from "react-router-dom";
 import './main.css';
-import { from } from 'rxjs';
+
+// 自定义链接样式
+function Menu(props) {
+    let {to, mname} = props;
+    return (
+        <Route path={to} children={({match})=>{
+            // 匹配路径后,math是对象, 否则为null
+            let iconClass = 'iconfont icon-happy2';
+            iconClass = match? iconClass + ' active': iconClass;
+            return (
+                <Link to= {to}>
+                    <div className="placeholder">
+                        <i className={iconClass}></i>
+                        <div className={match?'active':''}>{mname}</div>
+                    </div>
+                </Link>
+            )
+        }}/>
+    );
+}
 
 class Main extends React.Component {
     render () {
         return (
-            <div className='home-container'> 
+            <div className='home-container'>  
                 <div className='home-content'>
                     <Switch>
                         <Route path='/home/main' render={()=><div>home</div>}/>
                         <Route path='/home/info' render={()=><div>info</div>}/>
-                        <Route path='/home/chait' render={()=><div>chat</div>}/>
+                        <Route path='/home/chat' render={()=><div>chat</div>}/>
                         <Route path='/home/my' render={()=><div>my</div>}/>
                     </Switch>
                 </div>
@@ -21,36 +40,16 @@ class Main extends React.Component {
                    <Grid columns={4} divided>
                         <Grid.Row> 
                             <Grid.Column>
-                                <Link to= '/home/main'>
-                                    <div className="placeholder">
-                                        <i className='iconfont icon-happy2'></i>
-                                        <div>菜单</div>
-                                    </div>
-                                </Link>
+                                <Menu to='/home/main' mname='主页'/>
                             </Grid.Column>
                             <Grid.Column>
-                            <Link to= '/home/info'>
-                                <div className="placeholder">
-                                    <i className='iconfont icon-wink2'></i>
-                                    <div>菜单</div>
-                                </div>
-                            </Link>
+                                <Menu to='/home/info' mname='咨讯'/>
                             </Grid.Column>
                             <Grid.Column>
-                            <Link to= '/home/chait'>
-                                <div className="placeholder">
-                                    <i className='iconfont icon-grin'></i>
-                                    <div>菜单</div>
-                                </div>
-                            </Link>
+                                <Menu to='/home/chat' mname='微聊'/>
                             </Grid.Column>
                             <Grid.Column>
-                            <Link to= '/home/my'>
-                                <div className="placeholder">
-                                    <i className='iconfont icon-angry2'></i>
-                                    <div>菜单</div>
-                                </div>
-                            </Link>
+                                <Menu to='/home/my' mname='我的'/>
                             </Grid.Column>
                         </Grid.Row>
                     </Grid>
